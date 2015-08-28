d87f14f6e36d42032c8affa3aa9062b70568183b
tobias-dv-lnu <tobias.ohlsson@lnu.se>
2015-08-28 08:59:06 +0200

    Changed starting language to English (SimpleView)

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -10,7 +10,7 @@ namespace BlackJack
         static void Main(string[] args)
         {
             model.Game g = new model.Game();
-            view.IView v = new view.SwedishView();//new view.SimpleView();
+            view.IView v = new view.SimpleView(); // new view.SwedishView();
             controller.PlayGame ctrl = new controller.PlayGame();
 
             while (ctrl.Play(g, v));
